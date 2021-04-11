4370b04c2ef983cb71647a34777dd3e8faae0fed
bamata <subamamaurice@gmail.com>
2021-04-11 19:15:42 +0300

    Removed unnecessary js objects and some styles

--- client/src/scalableCard/Display.js
+++ client/src/scalableCard/Display.js
@@ -5,165 +5,85 @@ const showContent = function () {  //DISPLAY FUNCTION
     var style = document.createElement('style');
     style.innerHTML = `
     #Card {
-    background-color: #F0F8FF; /*lightblue*/;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-    width: 370px;
-    text-align: center;
-    border-radius: 8px;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 5px 9px;
+      background-color: #F0F8FF; /*lightblue*/;
+      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+      transition: 0.3s;
+      width: 370px;
+      text-align: center;
+      border-radius: 8px;
+      margin-left: auto;
+      margin-right: auto;
+      padding: 5px 9px;
     }    
+
     #card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-  }
-    #cardTitle {
-    color: #000033;
-    font-family:Verdana, Geneva, Tahoma, sans-serif;
-    font-size: 50px; /*xx-large*/;
+      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
-
-    #cardSubTitle{
-    color: #000033;
-    font-family:Verdana, Geneva, Tahoma, sans-serif;
-    font-size: small;
-    }
-
-    input[type=text], select, textarea {
-        width: 95%;
-        /*padding: 4px;*/
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        resize: vertical;
-        padding: 13px 10px;
-        margin: 9px 0;
-      }
-
-      input[type=text]:focus {
-        outline: none;
-        border: 0.09px dotted #444;
-      }
-      /* Updated */
-      form {
-        background-color: #fff;
-        border-radius: 3px;
-        padding: 7px;
-      }
-
-      input[type=submit] {
-        background-color: #fff ;/*#4CAF50*/;  /* Updated */
-        color: #444;  /* Updated */
-        padding: 10px 16px;
-        border: 2px solid lightblue;  /* Updated */
-        /*border-top: 2px;*/
-        border-radius: 1px;
-        cursor: pointer;
-        /*float: center;*/
-        width: 98%;  /* Updated */
-      
-      }
-      /* Updated */
-      input[type=submit]:hover {
-        color: #FFF;
-        background-color: lightblue;
-      }
-
-      .accordion {
-        background-color: #eee;
-        color: #444;
-        cursor: pointer;
-        padding: 18px;
-        text-align: left;
-        width: 100%;
-        outline: none;
-        transition: 0.4s;
-        border: 0;
-        height: 50px;
-        margin-top: 4px;
-      }
-
-      .active, .accordion:hover {
-        background-color: #ddd;
-      }
-
-      .accordion:after {
-        content: '\\002B'; /*002B*/
-        color: white;
-        font-weight: bold;
-        font-size: 25px;
-        margin-left: 5px;
-        float: right;
-        color: #444;
-      }
-
-      .active:after {
-        content: "\\2212"
-      }
-
-      .panel {
-        padding: 0 18px;
-        background-color: #F0F8FF;
-        display: none;
-        overflow: hidden;
-      
-      }
-
-      #btnContainer {
-        margin-top: 15px;
-        width: 100%;
-        /*
-        display: flex;
-        justify-content: space-around;
-        */
-      }
-
-     
     
-        #btnSignup, #btnLogin {
-          /*background-color: #F0F8FF;*/
-          border: none;
-          padding: 16px 20px;
-          text-align: center;
-          text-decoration: none;
-          display: inline-block;
-          font-size: 12.5px;
-          margin: 4px 2px;
-          transition-duration: 0.4s;
-          cursor: pointer;
-          border-radius: 4px;
-         
-      }
-      #btnSignup {
-        
-        background-color: #b3ecff;
-      }
+    #cardTitle {
+      color: #000033;
+      font-family:Verdana, Geneva, Tahoma, sans-serif;
+      font-size: 50px; /*xx-large*/;
+    }
 
-      #btnSignup:hover {
-        background-color: #99e6ff; /*#4CAF50*/;
-        /*color: white*/;
-      }
-     
-      #btnLogin:hover {
-        background-color: #b3b3b3; /*#4CAF50*/;
+    #cardSubTitle {
+      color: #000033;
+      font-family:Verdana, Geneva, Tahoma, sans-serif;
+      font-size: small;
 
-      }
+    }
+      
+    .accordion {
+      background-color: #eee;
+      color: #444;
+      cursor: pointer;
+      padding: 18px;
+      text-align: left;
+      width: 100%;
+      outline: none;
+      transition: 0.4s;
+      border: 0;
+      height: 50px;
+      margin-top: 10px;
+    }
 
-      #btnSignup, #btnLogin:focus {
-        outline: none;
-      }
+    .active, .accordion:hover {
+      background-color: #ddd;
+    }
 
-      #signup_with_google {
-        width: 100%px;
-        height: 50px;
-      }
+    .accordion:after {
+      content: "\\002B";
+      font-weight: bold;
+      font-size: 25px;
+      margin-left: 5px;
+      float: right;
+      color: grey; 
+    }
 
-     #signup_with_google:hover {
+    .active:after {
+      content: "\\2212"
+    }
+
+    .panel {
+      padding: 0 18px;
+      background-color: #F0F8FF;
+      display: none;
+      overflow: hidden;  
+    }
+
+    #btnContainer {
+      margin-top: 15px;
+      width: 100%;
+    }
+    
+    #signup_with_google {
+      width: 100%px;
+      height: 50px;
+    }
+
+    #signup_with_google:hover {
         cursor: pointer;
-      }
-      
-      
-      
+    }
       
     `;
     document.head.appendChild(style);
@@ -181,7 +101,6 @@ const showContent = function () {  //DISPLAY FUNCTION
     rootDisplay.appendChild(h1);
 
     document.getElementsByTagName("H1")[0].setAttribute("id", "cardTitle");
-
     //END TITLE SECTION
 
     //SUB-TITLE SECTION
@@ -190,11 +109,7 @@ const showContent = function () {  //DISPLAY FUNCTION
     rootDisplay.appendChild(h3);
 
     document.getElementsByTagName("H3")[0].setAttribute("id", "cardSubTitle");
-
     //END SUB-TITLE SECTION
-
-
-    //MAPPING DATA SECTION
 
     let btnAccordionOne, 
         btnAccordionTwo, 
@@ -253,7 +168,6 @@ const showContent = function () {  //DISPLAY FUNCTION
           console.log("clicked");
         });
     }
-    //END MAPPING DATA SECTION
     //END ACCORDION SECTION
 
     //SIGN UP AND LOGIN BUTTONS
@@ -262,107 +176,11 @@ const showContent = function () {  //DISPLAY FUNCTION
     img.src = "./signup_with_google.jpg";
     img.setAttribute('id', 'signup_with_google');
 
-    btnSignUp = createElement("button");
-    btnLogin = createElement("button");
     buttonsContainer = createElement("div");
-
-    btnSignUp.setAttribute("class", "btn_signin_login");
-    btnSignUp.setAttribute("id", "btnSignup");
-    btnLogin.setAttribute("id", "btnLogin");
     buttonsContainer.setAttribute("id", "btnContainer");
-    btnLogin.setAttribute("class", "btn_signin_login");
 
-    btnSignUp.innerHTML = "SIGN UP";
-    btnLogin.innerHTML  = "LOGIN IN";
-
-    // buttonsContainer.appendChild(btnSignUp);
-    // buttonsContainer.appendChild(btnLogin);
     buttonsContainer.appendChild(img);
     rootDisplay.appendChild(buttonsContainer);
-
-
-    //FORM SECTION
-    const form = createElement('form');
-
-    form.setAttribute("method", "post");
-    form.style.marginTop = "15px";
-
-    const fullName = createElement("input");
-    fullName.setAttribute("type", "text");
-    fullName.setAttribute("name", "FullName");
-    fullName.setAttribute("placeholder", "Full Name");
-
-
-    const lastName = createElement("input");
-    lastName.setAttribute("type", "text");
-    lastName.setAttribute("name", "dob");
-    lastName.setAttribute("placeholder", "Last Name");
-
-    const streetAddress = createElement("input")
-    streetAddress.setAttribute("type", "text");
-    streetAddress.setAttribute("name", "streetAddr");
-    streetAddress.setAttribute("placeholder", "Street Address");
-
-    const zip = createElement("input")
-    zip.setAttribute("type", "text");
-    zip.setAttribute("name", "zip");
-    zip.setAttribute("placeholder", "zip");
-
-    const phone = createElement("input")
-    phone.setAttribute("type", "text");
-    phone.setAttribute("name", "phone");
-    phone.setAttribute("placeholder", "Telephone");
-
-    const email = createElement("input")
-    email.setAttribute("type", "text");
-    email.setAttribute("name", "email");
-    email.setAttribute("placeholder", "Email");
-
-
-
-    const btnSubmit = createElement("input");
-    btnSubmit.setAttribute("type", "submit");
-    btnSubmit.setAttribute("value", "Submit");
-
-
-
-
-    form.appendChild(fullName);
-
-    const br = createElement('br');
-
-
-    form.appendChild(br.cloneNode());
-
-
-    form.appendChild(lastName);
-    form.appendChild(br.cloneNode());
-
-    form.appendChild(streetAddress)
-    form.appendChild(br.cloneNode());
-
-    form.appendChild(zip)
-    form.appendChild(br.cloneNode());
-
-    form.appendChild(phone)
-    form.appendChild(br.cloneNode());
-
-    form.appendChild(email)
-    form.appendChild(br.cloneNode());
-
-    form.appendChild(btnSubmit);
-
-    //rootDisplay.appendChild(form);
-
-    //FORM SECTION END
-
-    btnSignUp.addEventListener("click", function(){
-      rootDisplay.appendChild(form);
-      btnAccordionOne.style.display = "none";
-      btnAccordionTwo.style.display = "none";
-      buttonsContainer.style.display = "none";
-    });
-
 
 }
 
@@ -372,6 +190,4 @@ const createElement = function (element) {
 }
 
 showContent();
-
-//export { createElement };
 
